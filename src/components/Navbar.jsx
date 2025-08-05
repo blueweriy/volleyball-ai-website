9d6c17e57872c959624aac46cf6222229e2ee1e9
blueweriy <harry.why0216@gmail.com>
2025-08-05 11:09:10 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <header className="sticky top-0 bg-white border-b border-gray-200 z-50">
-      <nav className="max-w-6xl mx-auto flex items-center justify-between h-16 px-4">
-        {/* Text Logo */}
+      <nav className="max-w-6xl mx-auto flex items-center justify-start h-16 px-4 space-x-12">
+        {/* Logo */}
         <Link
           to="/"
           className="text-2xl font-bold text-gray-900 hover:text-gray-700 no-underline"
@@ -12,7 +12,7 @@ export default function Navbar() {
           VolleyballAI
         </Link>
 
-        {/* Center nav links */}
+        {/* Now the links sit right next to it */}
         <ul className="flex space-x-8">
           {[
             ["Home", "/"],
