7549eb67b92a151c0fa13fa652ccc8359ce4d7fe
blueweriy <harry.why0216@gmail.com>
2025-08-07 22:25:03 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,69 @@
 // src/App.jsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
+// top-level pages
+import Home from './pages/Home';
+import Workout from './pages/Workout';
+import Purchases from './pages/Purchases';
 
-// Top‐level pages
-import Home from "./pages/Home";
-import Analysis from "./pages/Analysis";
-import Callouts from "./pages/Callouts";
-import Purchases from "./pages/Purchases";
-import Rules from "./pages/Rules";
-import Skills from "./pages/Skills";
-import Workout from "./pages/Workout";
+// workout pages
+import Plyometrics from './pages/workout/Plyometrics';
+import Strength     from './pages/workout/Strength';
+import Stamina      from './pages/workout/Stamina';
+import Injury       from './pages/workout/Injury';
 
-// Skill sub-pages
-import Serve    from "./pages/skillpages/Serve.jsx";
-import Set      from "./pages/skillpages/Set.jsx";
-import Receive  from "./pages/skillpages/Receive.jsx";
-import Dig      from "./pages/skillpages/Dig.jsx";
-import Spike    from "./pages/skillpages/Spike.jsx";
-import Block    from "./pages/skillpages/Block.jsx";
+// purchase detail pages
+import NetsBudget    from './pages/purchases/NetsBudget';
+import NetsStandard  from './pages/purchases/NetsStandard';
+import NetsPremium   from './pages/purchases/NetsPremium';
+import ShoesBudget   from './pages/purchases/ShoesBudget';
+import ShoesStandard from './pages/purchases/ShoesStandard';
+import ShoesPremium  from './pages/purchases/ShoesPremium';
+import BallsBudget   from './pages/purchases/BallsBudget';
+import BallsStandard from './pages/purchases/BallsStandard';
+import BallsPremium  from './pages/purchases/BallsPremium';
+import KneePads      from './pages/purchases/KneePads';
+import AnkleBraces   from './pages/purchases/AnkleBraces';
+import Cones         from './pages/purchases/Cones';
 
-// Workout sub-pages
-import Plyometrics from "./pages/workout/Plyometrics.jsx";
-import Strength    from "./pages/workout/Strength.jsx";
-import Stamina     from "./pages/workout/Stamina.jsx";
-import Injury      from "./pages/workout/Injury.jsx";
-
-export default function App() {
+function App() {
   return (
-    <>
-      <Navbar />
-      <main className="bg-white min-h-screen">
-        <Routes>
-          {/* Top-level */}
-          <Route path="/" element={<Home />} />
-          <Route path="/analysis" element={<Analysis />} />
-          <Route path="/callouts" element={<Callouts />} />
-          <Route path="/purchases" element={<Purchases />} />
-          <Route path="/rules" element={<Rules />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/workout" element={<Workout />} />
+    <BrowserRouter>
+      <Routes>
+        {/* main */}
+        <Route path="/" element={<Home />} />
+        <Route path="/workout" element={<Workout />} />
+        <Route path="/purchases" element={<Purchases />} />
 
-          {/* Skill detail */}
-          <Route path="/skills/serve"   element={<Serve />} />
-          <Route path="/skills/set"     element={<Set />} />
-          <Route path="/skills/receive" element={<Receive />} />
-          <Route path="/skills/dig"     element={<Dig />} />
-          <Route path="/skills/spike"   element={<Spike />} />
-          <Route path="/skills/block"   element={<Block />} />
+        {/* workout sub-routes */}
+        <Route path="/workout/plyometrics" element={<Plyometrics />} />
+        <Route path="/workout/strength"     element={<Strength />} />
+        <Route path="/workout/stamina"      element={<Stamina />} />
+        <Route path="/workout/injury"       element={<Injury />} />
 
-          {/* Workout detail */}
-          <Route path="/workout/plyometrics" element={<Plyometrics />} />
-          <Route path="/workout/strength"    element={<Strength />} />
-          <Route path="/workout/stamina"     element={<Stamina />} />
-          <Route path="/workout/injury"      element={<Injury />} />
+        {/* purchases sub-routes */}
+        <Route path="/purchases/nets/budget"   element={<NetsBudget />} />
+        <Route path="/purchases/nets/standard" element={<NetsStandard />} />
+        <Route path="/purchases/nets/premium"  element={<NetsPremium />} />
 
-          {/* Fallback 404 could go here */}
-        </Routes>
-      </main>
-    </>
+        <Route path="/purchases/shoes/budget"   element={<ShoesBudget />} />
+        <Route path="/purchases/shoes/standard" element={<ShoesStandard />} />
+        <Route path="/purchases/shoes/premium"  element={<ShoesPremium />} />
+
+        <Route path="/purchases/balls/budget"   element={<BallsBudget />} />
+        <Route path="/purchases/balls/standard" element={<BallsStandard />} />
+        <Route path="/purchases/balls/premium"  element={<BallsPremium />} />
+
+        <Route path="/purchases/accessories/knee-pads"   element={<KneePads />} />
+        <Route path="/purchases/accessories/ankle-braces" element={<AnkleBraces />} />
+        <Route path="/purchases/accessories/cones"        element={<Cones />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
