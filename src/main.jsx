66c94899bf35e2cbcc94ec0acbd12a383dc13bf7
blueweriy <harry.why0216@gmail.com>
2025-08-07 22:38:12 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-// THIS is how you import CSS—no CSS imports inside the CSS file itself
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
