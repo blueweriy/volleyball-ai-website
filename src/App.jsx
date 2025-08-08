56ec904cd85bec433700e4ae591793370062b9d9
blueweriy <harry.why0216@gmail.com>
2025-08-07 22:28:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // Top-level pages
 import Home      from './pages/Home';
+import Rules     from './pages/Rules';
+import Callouts  from './pages/Callouts';
 import Workout   from './pages/Workout';
 import Purchases from './pages/Purchases';
 
-// Workout detail pages
+// Workout sub-pages
 import Plyometrics from './pages/workout/Plyometrics';
 import Strength    from './pages/workout/Strength';
 import Stamina     from './pages/workout/Stamina';
 import Injury      from './pages/workout/Injury';
 
-// Purchases detail pages
+// Purchases sub-pages
 import NetsBudget    from './pages/purchases/NetsBudget';
 import NetsStandard  from './pages/purchases/NetsStandard';
 import NetsPremium   from './pages/purchases/NetsPremium';
@@ -31,38 +33,37 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Main routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/workout" element={<Workout />} />
-        <Route path="/purchases" element={<Purchases />} />
+        {/* Main pages */}
+        <Route path="/"                element={<Home />} />
+        <Route path="/rules"           element={<Rules />} />
+        <Route path="/callouts"        element={<Callouts />} />
+        <Route path="/workout"         element={<Workout />} />
+        <Route path="/purchases"       element={<Purchases />} />
 
-        {/* Workout sub-routes */}
+        {/* Workout detail routes */}
         <Route path="/workout/plyometrics" element={<Plyometrics />} />
         <Route path="/workout/strength"     element={<Strength />} />
         <Route path="/workout/stamina"      element={<Stamina />} />
         <Route path="/workout/injury"       element={<Injury />} />
 
-        {/* Purchases sub-routes: Nets */}
+        {/* Purchases detail routes */}
         <Route path="/purchases/nets/budget"   element={<NetsBudget />} />
         <Route path="/purchases/nets/standard" element={<NetsStandard />} />
         <Route path="/purchases/nets/premium"  element={<NetsPremium />} />
 
-        {/* Purchases sub-routes: Shoes */}
         <Route path="/purchases/shoes/budget"   element={<ShoesBudget />} />
         <Route path="/purchases/shoes/standard" element={<ShoesStandard />} />
         <Route path="/purchases/shoes/premium"  element={<ShoesPremium />} />
 
-        {/* Purchases sub-routes: Balls */}
         <Route path="/purchases/balls/budget"   element={<BallsBudget />} />
         <Route path="/purchases/balls/standard" element={<BallsStandard />} />
         <Route path="/purchases/balls/premium"  element={<BallsPremium />} />
 
-        {/* Purchases sub-routes: Accessories */}
         <Route path="/purchases/accessories/knee-pads"   element={<KneePads />} />
         <Route path="/purchases/accessories/ankle-braces" element={<AnkleBraces />} />
         <Route path="/purchases/accessories/cones"        element={<Cones />} />
 
-        {/* Fallback: redirect unknown routes back to home */}
+        {/* Catch-all */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
